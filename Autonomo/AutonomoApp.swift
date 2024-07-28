7607996aad927786869fda64024977840159e216
metemuhammetturan <mete3381@gmail.com>
2024-07-28 17:15:22 +0300

    Initial Commit

---
+++ Autonomo/AutonomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutonomoApp.swift
+//  Autonomo
+//
+//  Created by Mete Turan on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AutonomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
